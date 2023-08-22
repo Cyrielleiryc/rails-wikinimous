5e5cc09cacc150531bfbac66eca57d711be9f43e
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-22 15:20:28 +0200

    seed and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating articles...'
+10.times do
+  a = Article.new(title: Faker::Movie.title, content: Faker::Quote.famous_last_words)
+  a.save if a.valid?
+end
+puts 'Finished!'
